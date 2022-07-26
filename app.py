9a47073db2e6a98a2d5b65cb37818ac7e747253e
Matheus S <matheus.silvamps75@gmail.com>
2022-07-26 23:22:00 +0000

    Segundo commit

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
 from flask import Flask, render_template
+from datetime import datetime
  
 app = Flask("'hello'")
 
+posts = [
+    {
+        'title': "primeiro post",
+        'body': "texto do post",
+        'author': "autor",
+        'created': datetime(2022,7,25)
+    }
+]
+
 @app.route("/")
-def hello():
-    return render_template('index.html') 
+@app.route('/home')
+def index():
+    return render_template('index.html', posts = posts) 
